ed4062965611375dcaba5ecd37d6032b3dd1d77b
TP-xx <t.perkert@gmx.de>
2025-01-28 12:28:33 +0100

    up

--- src/main/java/com/geoproject/libraries/CountryLibrary.java
+++ src/main/java/com/geoproject/libraries/CountryLibrary.java
@@ -4,27 +4,34 @@ package com.geoproject.libraries;
 //Author: Theodor
 //Version: 21/1/2025
 
-public class CountryLibrary {
-
-    
-    //bitte noch preis hinzufügen
-
-
+public class CountryLibrary
+{
     @SuppressWarnings("unused")
     public static int[][] borders = new int[][] {
+        {0, 0, 0, 0, 0, 0},//NULL
         {2, 3, 4, 5, 6, 7},//DE
         {1, 3, 5, 8, 0, 0},//BENELUX
-        {1, 2, 4, 9, 10, 0},//DEN
+        {1, 2, 4, 9, 19, 23},//DEN
         {1, 3, 6, 10, 12, 13},//POL
         {1, 2, 7, 8, 14, 15},//FRA
-        {1, 0, 0, 0, 0, 0},//CZE
+        {1, 4, 7, 12, 0, 0},//CZE
+        {1, 5, 6, 12, 16, 17},//SWI/AU
+        {2, 5, 18, 19, 20, 0},//UK
+        {3, 4, 10, 11, 21, 0},//SCAN
+        {4, 9, 11, 21, 22, 0},//BALT
+        {9, 10, 21, 0, 0, 0},//FIN
+        {4, 6, 7, 13, 17, 0},//SLOVA
+        {4, 12, 17, 21, 22, 24},//UKR
     };
 
     @SuppressWarnings("unused")
-    public static String[] countryNames = new String[] {"DE", "BENELUX", "DEN", "POL", "FRA", "CZE"};
+    public static String[] countryNames = new String[] {"Kein Land", "Deutschland", "BeNeLux", "Dänemark", "Polen", "Frankreich",
+            "Tschechien", "Österreich und Schweiz", "Vereinigtes Königreich", "Skandinavien", "Baltikum", "Finnland",
+            "Slowakei", "Ukraine", "Spanien", "Italien", "Slowenien", "Ungarn", "Irland", "Island", "Vereinigte Staaten",
+            "Russland", "Belarus", "Kanada", "Romänien"};
 
     @SuppressWarnings("unused")
-    public static String[][] statNames = new String[][] {
+    public static String[][] StatNames = new String[][] {
         {"Bergbau", "Bb"},
         {"Landwirtschaft", "Lw"},
         {"Warenproduktion", "Wp"},
