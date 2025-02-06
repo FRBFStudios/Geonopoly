d25622b4026b6911f441dbcdc81c44e7c9d36679
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-02-06 17:36:13 +0100

    countryNames-String ergänzt

--- src/main/java/com/geoproject/libraries/CountryLibrary.java
+++ src/main/java/com/geoproject/libraries/CountryLibrary.java
@@ -6,52 +6,32 @@ package com.geoproject.libraries;
 
 public class CountryLibrary
 {
-    @SuppressWarnings("unused")
+    //Beinhält die Grenzländer aller Länder (muss komplett neu ausgefüllt werden)
     public static int[][] borders = new int[][] {
-        {2, 3, 4, 5, 6, 7},//DE - 1
-        {1, 3, 5, 8, 0, 0},//BENELUX - 2
-        {1, 2, 4, 9, 19, 23},//DEN - 3
-        {1, 3, 6, 10, 12, 13},//POL - 4
-        {1, 2, 7, 8, 14, 15},//FRA - 5
-        {1, 4, 7, 12, 0, 0},//CZE - 6
-        {1, 5, 6, 12, 16, 17},//SWI/AU - 7
-        {2, 5, 18, 19, 20, 0},//UK - 8
-        {3, 4, 10, 11, 21, 0},//SCAN - 9
-        {4, 9, 11, 21, 22, 0},//BALT - 10
-        {9, 10, 21, 0, 0, 0},//FIN - 11
-        {4, 6, 7, 13, 17, 0},//SLOVA - 12
-        {4, 12, 17, 21, 22, 24},//UKR - 13
-        {0, 0, 0, 0, 0, 0},//SPA - 14
-        {0, 0, 0, 0, 0, 0},//ITA - 15
-        {0, 0, 0, 0, 0, 0},//SLO - 16
-        {0, 0, 0, 0, 0, 0},//HUN - 17
-        {0, 0, 0, 0, 0, 0},//IRE - 18
-        {0, 0, 0, 0, 0, 0},//ICE - 19
-        {0, 0, 0, 0, 0, 0},//USA - 20
-        {0, 0, 0, 0, 0, 0},//RUS - 21
-        {0, 0, 0, 0, 0, 0},//BEL - 22
-        {0, 0, 0, 0, 0, 0},//CAN - 23
-        {0, 0, 0, 0, 0, 0},//ROM - 24
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
-        {0, 0, 0, 0, 0, 0},//
+        {1, 2, 4, 5, 6, 7},//Deutschland - 0
+        {0, 2, 5, 8},//BeNeLux - 1
+        {0, 1, 4, 9, 19, 23},//Dänemark - 2
+        {0, 2, 6, 10, 12, 13},//Polen - 3
+        {0, 1, 7, 8, 14, 15},//Frankreich - 4
+        {0, 3, 7, 12},//Tschechien - 5
+        {0, 4, 6, 12, 16, 17},//Schweiz & Österreich - 6
+        {1, 4, 18, 19, 20},//Vereinigtes Königreich - 7
+        {2, 3, 10, 11, 21},//Skandinavien [Norwegen und Schweden] - 8
+        {3, 8, 11, 21, 22},//Baltikum - 9
+        {8, 9, 21},//Finnland - 10
+        {3, 5, 7, 13, 17},//Slowakei - 11
+        {},//Belarus - 12
+        {2, 11, 17, 21, 22, 24},//Ukraine - 13
     };
 
-    @SuppressWarnings("unused")
     public static int statsUpgradeCosts = 100; //Kosten für das Upgraden der Stats
 
-    @SuppressWarnings("unused")
-    public static int[] countryPrice = new int[] //Bitte ausfüllen
-        {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
+    public static int getCountryPrice(int countryID) {
+        int price = 0;
+        return price;
+    }
 
-    @SuppressWarnings("unused")
+    //Beinhält die StatsMultiplier der Industrien
 	public static int[][] statsMultiplier = new int[][] {
         {1, 1, 1, 1, 1, 1, 1},//DE
         {1, 1, 1, 1, 1, 1, 1},//BENELUX
@@ -77,21 +57,24 @@ public class CountryLibrary
         {1, 1, 1, 1, 1, 1, 1},//BEL
         {1, 1, 1, 1, 1, 1, 1},//CAN
         {1, 1, 1, 1, 1, 1, 1},//ROM
-	};  //bitte Vervollständigen (werte von 1 bis 5 wären wahrscheinlich sinnvoll)!
+	};
 
-    @SuppressWarnings("unused")
-    public static String[] countryNames = new String[] {"Deutschland", "BeNeLux", "Dänemark", "Polen", "Frankreich",
-            "Tschechien", "Österreich und Schweiz", "Vereinigtes Königreich", "Skandinavien", "Baltikum", "Finnland",
-            "Slowakei", "Ukraine", "Spanien", "Italien", "Slowenien", "Ungarn", "Irland", "Island", "Vereinigte Staaten",
-            "Russland", "Belarus", "Kanada", "Romänien"};
+    //Array, der die Namen der Länder beinhält
+    public static String[] countryNames = new String[] {"Deutschland", "BeNeLux-Ländergruppe", "Dänemark", "Polen", "Frankreich",
+            "Tschechien", "Österreich und Schweiz", "Vereinigtes Königreich", "Skandinavische Ländergruppe", "Baltikum", "Finnland",
+            "Slowakei", "Belarus", "Ukraine", "Italien", "Balkanländergruppe", "Ungarn", "Romänien", "Griechenland und Makedonien",
+            "Vereinigte Staaten", "Kanada", "Mexiko", "Mittelamerikanische Ländergruppe", "Kolumbien", "Venezuela", "Brasilien", "Ecuador",
+            "Peru", "Guyana", "Suriname", "Argentinien", "Chile", "Grönland", "Bolivien", "Paraguay", "Uruguay", "Island", "Irland", "Russland",
+            "Türkei", "Syrien", "Jordanien", "Israel und Palästina", "Beirut", "Saudi-Arabien", "Jemen", "Oman", "Spanien", "VAE und Katar",
+            "Irak", "Georgien, Armenien und Aserbaidschan", "Bulgarien", "Iran", "Pakistan", "Kasachstan", "Usbekistan", "Turkmenistan",
+            "Tadschikistan", "Afghanistan", "Kirgistan", "Indien", "Burma", "Bangladesch", "Nepal", "Kuba", "Sri Lanka", "Mongolei", "China"};
 
-    //Ich glaube, das kann weg
-    @SuppressWarnings("unused")
+    //Listet alle Industrietypen
     public static String[][] statNames = new String[][] {
-        {"Bergbau", "Bb"},
-        {"Landwirtschaft", "Lw"},
-        {"Warenproduktion", "Wp"},
-        {"Maschinenbau", "Mb"},
+        {"Bergbau", "BB"},
+        {"Landwirtschaft", "LW"},
+        {"Warenproduktion", "WP"},
+        {"Maschinenbau", "MB"},
         {"Tourismus", "T"},
         {"Energie", "E"},
         {"Wissenschaft", "W"}
